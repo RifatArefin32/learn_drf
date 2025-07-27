b5e0f7bff63f84aed78c5a134db9745640657d5f
RifatArefin32 <arefinrifat786@gmail.com>
2025-07-27 21:58:21 +0600

    class-5: optimize order list view

--- apps/core/views.py
+++ apps/core/views.py
@@ -42,7 +42,7 @@ def product_details_with_shortcut(request, id):
 # Show all orders
 @api_view(["GET"])
 def order_list(request):
-    orders = Order.objects.all()
+    orders = Order.objects.prefetch_related('items', 'items__product').all()
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
 
