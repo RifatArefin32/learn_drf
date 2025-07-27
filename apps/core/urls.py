88632d9bc79178336d5dd0426cf4cc6fbfbb5411
RifatArefin32 <arefinrifat786@gmail.com>
2025-07-27 17:42:24 +0600

    Product List With Django Shortcut

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,5 +3,6 @@ from apps.core import views
 
 urlpatterns = [
     path('products/', views.product_list),
+    path('products_shortcut/', views.product_list_with_shortcut),
     path('products/<int:id>/', views.product_details),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -24,3 +24,8 @@ def product_details(request, id):
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+@api_view(['GET'])
+def product_list_with_shortcut(request):
+    products = get_list_or_404(Product)
+    serializer = ProductSerailizer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
