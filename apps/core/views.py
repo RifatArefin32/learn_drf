b7b7057d8130294445be54b7c487385619ed04bd
RifatArefin32 <arefinrifat786@gmail.com>
2025-08-01 22:33:17 +0600

    class-11: add permission class in product create list api view

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,7 +2,7 @@ from django.db.models import Max, Min
 from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from apps.core.models import Product, Order
@@ -46,6 +46,11 @@ class ProductCreateApiView(generics.CreateAPIView):
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer 
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 
 # Product details
